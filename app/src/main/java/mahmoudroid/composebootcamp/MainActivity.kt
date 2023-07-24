7437a48e5ab05b251b83ec4ae81cabcee9888d3e
Mahmoudroid <mahmoudroid@Mahmoudrezas-MacBook-Pro.local>
2023-07-24 18:19:06 +0330

    add scaffold and topbar

--- app/src/main/java/mahmoudroid/composebootcamp/MainActivity.kt
+++ app/src/main/java/mahmoudroid/composebootcamp/MainActivity.kt
@@ -1,43 +1,75 @@
 package mahmoudroid.composebootcamp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import mahmoudroid.composebootcamp.ui.theme.ComposeBootcampTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBootcampTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello from $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeBootcampTheme {
+        Scaffold(
+            
+            topBar = {
+                Surface(shadowElevation = 4.dp) {
+                    CenterAlignedTopAppBar(
+                        title = { Text(text = "Movies") },
+                        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                            containerColor = Color.Magenta,
+                        ),
+                    )
+                }
+            },
+
+
+        ){
+           Box(modifier = Modifier.padding(it)){
+               content.invoke()
+           }
+        }
+    }
 }
+
+@Composable
+fun MainContent() {
+    Text(text = "Hellog")
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeBootcampTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
