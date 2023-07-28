8587b50db104b5e9090bfd028b9a3300e392d69f
Mahmoudroid <mahmoudroid@Mahmoudrezas-MacBook-Pro.local>
2023-07-28 18:18:33 +0330

    add AnimatedVisibility to movieRowItem

--- app/src/main/java/mahmoudroid/composebootcamp/widgets/widgets.kt
+++ app/src/main/java/mahmoudroid/composebootcamp/widgets/widgets.kt
@@ -1,18 +1,18 @@
 package mahmoudroid.composebootcamp.widgets
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -20,6 +20,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,11 +32,15 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
 import mahmoudroid.composebootcamp.R
 import mahmoudroid.composebootcamp.model.Movie
 import mahmoudroid.composebootcamp.model.getMovies
@@ -44,9 +52,13 @@ fun MovieRowItem(
     item: Movie = getMovies()[0],
     clickCallback: (String) -> Unit = {}
 ) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(modifier = Modifier
         .fillMaxWidth()
-        .height(130.dp)
         .clickable {
             clickCallback.invoke(item.id)
         }
@@ -86,6 +98,45 @@ fun MovieRowItem(
                 Text(text = item.title, style = MaterialTheme.typography.headlineSmall)
                 Text(text = "Director : ${item.director}", style = MaterialTheme.typography.labelSmall)
                 Text(text = "Released: ${item.year}", style = MaterialTheme.typography.labelSmall)
+
+                AnimatedVisibility(visible = expanded) {
+                    
+                    Column {
+                        Text(text = buildAnnotatedString {
+
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp
+                                )
+                            ){
+                                append(" Plot: ")
+                            }
+
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ){
+                                append(item.plot)
+                            }
+
+                        })
+                    }
+                    
+                }
+
+                Icon(imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "",
+                    tint = Color.DarkGray,
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    )
             }
 
 
