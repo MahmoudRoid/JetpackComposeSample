8ee2de692cb67719c398cd2967d5bfacf945db81
Mahmoudroid <mahmoudroid@Mahmoudrezas-MacBook-Pro.local>
2023-08-04 18:11:57 +0330

    complete movie app

--- app/src/main/java/mahmoudroid/composebootcamp/screens/DetailScreen.kt
+++ app/src/main/java/mahmoudroid/composebootcamp/screens/DetailScreen.kt
@@ -1,19 +1,25 @@
 package mahmoudroid.composebootcamp.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,14 +28,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import mahmoudroid.composebootcamp.model.Movie
+import mahmoudroid.composebootcamp.model.getMovies
+import mahmoudroid.composebootcamp.widgets.MovieRowItem
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DetailScreen(navController: NavController, movieData: String?) {
+fun DetailScreen(navController: NavController, movieId: String?) {
+
+    // filter movie with desired movie id
+    val newMovieList = getMovies().filter { item -> item.id == movieId }
 
     Scaffold(
 
@@ -49,35 +64,56 @@ fun DetailScreen(navController: NavController, movieData: String?) {
             }
         },
 
-    ) {
+        ) {
 
         Surface(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
         )
         {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(text = movieData ?: "nothing is passed", style = MaterialTheme.typography.headlineLarge)
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(onClick = {
-                    navController.navigateUp()
-                }) {
-                    Text(text = "go back")
-                }
+
+                MovieRowItem(newMovieList.first())
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Divider()
+
+                Text(text = "Movie Images")
+
+                HorizontalScrollableImageView(newMovieList)
+
             }
 
         }
 
     }
 
+}
 
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(items = newMovieList.first().images) { image ->
 
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.elevatedCardElevation(4.dp)
+            ) {
 
+                AsyncImage(
+                    model = image,
+                    contentDescription = "",
+                    contentScale = ContentScale.FillBounds
+                )
 
-
-
-
-
+            }
+        }
+    }
 }
